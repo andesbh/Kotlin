0e5f7c184698247d1f8ba10a7012a1ae0a430afe
Anderson <andersonlucascs@yahoo.com.br>
2022-10-31 17:32:57 -0300

    Iniciando no Kotlin2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 
 
-    var nome: String = "test";
+    var nome = "test";
     var sobrenome: String = "2";
 
     println("$nome $sobrenome");
@@ -17,7 +17,6 @@ fun main(args: Array<String>) {
     //Switch case == When
 
     var testeWhen: String = "";
-
     testeWhen = when(nome){
         "teste" -> "ok"
         "teste2" -> "2"
@@ -29,17 +28,56 @@ fun main(args: Array<String>) {
     //For
     for(i in 1..10){
         println(i.toString());
+        continue;
+
     }
 
     //For de 2 em 2
     for(i in 0..10 step 2){
         println(i.toString());
+        break
     }
 
     //For decrescente
     for(i in 20 downTo  0 ){
         println(i.toString());
     }
+
+    //Val = constante
+    val str: String? = null;
+    var str2: String? = "1234";
+
+  //Operador Elvis -> parecido, não igual ao If (em linha do vb.net) ou o ternário do c# (teste=="1234" ? "2" : "4";) só que valida apenas se o dado é nulo
+
+    println(str ?: "nulo");
+    println(str2 ?: "nulo");
+
+
+    //Expressão que se aproxima do ternário em Kotlin é parecido com o código em VB.net
+
+    val idade: Int = 18;
+    val str3 = if(idade >= 18) "Maior" else "Menor";
+
+    println(str3);
+
+
+    //let
+
+    val str5: String? = null;
+
+    if(str5 != null){
+        str5.lowercase();
+        str5.length;
+    }
+    // o let irá fazer a mesma coisa acima, só que não precisamos fazer a validação do nulo
+
+    str5?.let{
+        it.lowercase();
+        it.length;
+    }
+
+
+
 
 }
 
